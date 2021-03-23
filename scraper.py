cc93ffda2c58bae7c39bf225e4dcffa0b6098848
Angel Vicente SAnjuan Martin <av_sanjuan@telefonica.net>
2021-03-23 21:31:18 +0100

    23032021-1

--- scraper.py
+++ scraper.py
@@ -5,3 +5,10 @@ Editor de Spyder
 Este es un archivo temporal
 """
 
+import urllib2
+import re
+import time
+from bs4 import BeautifulSoup
+from dateutil import parser
+from geopy.geocoders import Yandex
+from reason_classifier import ReasonClassifier
