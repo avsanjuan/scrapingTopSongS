ee7a407e0428e405eade505b56b7ec539e3553d3
Angel Vicente SAnjuan Martin <av_sanjuan@telefonica.net>
2021-03-24 21:49:39 +0100

    24032021-2

--- scraper.py
+++ scraper.py
@@ -5,10 +5,51 @@ Editor de Spyder
 Este es un archivo temporal
 """
 
-import urllib2
+import urllib3
+import requests
 import re
 import time
 from bs4 import BeautifulSoup
 from dateutil import parser
-from geopy.geocoders import Yandex
-from reason_classifier import ReasonClassifier
+from requests.exceptions import HTTPError 
+
+#from reason_classifier import ReasonClassifier
+class Scraper():
+
+    def __init__(self):
+        self.url ="https://www.elportaldemusica.es/lists/top-100-canciones/2020"
+        self.subdomain = "/database.htm"
+        self.data = []
+        #self.geolocator = Yandex()
+        #self.reason_classifier = (
+        #ReasonClassifier("../train/summary_train_set.txt"))
+        
+        
+    def requestPage(url):  
+        try:
+            raw_html = requests.get(url)
+            try:
+                html = BeautifulSoup(raw_html.text, features='html.parser')
+                return html
+            except:
+                print('Error parsing HTML code')
+                return None
+        except HTTPError as e:
+            print(e.reason)
+            return None
+
+    def __download_html(self, url):
+        #self.response = urllib3.request(url)
+        #html = self.response.read()
+        html = self.requestPage()
+       # html = requestPage(url)
+        return html
+    
+    
+    def execute_scraper(self):
+        html = self.__download_html(self.url)
+        print('Hola mundo')
+        print(html)
+        
+
+    #print('hola mundo')
