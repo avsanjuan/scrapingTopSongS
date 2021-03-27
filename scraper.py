59d1d64452292ab530e7d972495b235c2031f31b
Angel Vicente SAnjuan Martin <av_sanjuan@telefonica.net>
2021-03-27 22:40:09 +0100

    27032021-1

--- scraper.py
+++ scraper.py
@@ -20,14 +20,10 @@ class Scraper():
         self.url ="https://www.elportaldemusica.es/lists/top-100-canciones/2020"
         self.subdomain = "/database.htm"
         self.data = []
-        #self.geolocator = Yandex()
-        #self.reason_classifier = (
-        #ReasonClassifier("../train/summary_train_set.txt"))
         
-        
-    def requestPage(url):  
+    def requestPage(self):  
         try:
-            raw_html = requests.get(url)
+            raw_html = requests.get(self.url, verify=False)
             try:
                 html = BeautifulSoup(raw_html.text, features='html.parser')
                 return html
@@ -47,9 +43,29 @@ class Scraper():
     
     
     def execute_scraper(self):
+        print(self.url)
+        print('paso 0')
         html = self.__download_html(self.url)
-        print('Hola mundo')
-        print(html)
+        print('paso 1')
+        if html is not None:
+            print('paso 2')
+        # Search wallpapers URL
+            ranking = html.find_all('p', {'class': 'single-list-entry-rank-position '})
+            print(ranking)
+            for rk in ranking:
+                i = rk.find('p')
+                print(i)
+                #img = wp.find('img')
+                #images.append(img.attrs['src'])
+    
+            # Search for next page URL
+            #try:
+            #    more_button = html.find('a', {'class':'more'})
+            #    next_page = more_button.attrs['href']
+            #except:
+            #    pass
+        #print('Hola mundo')
+        #print(html)
         
 
     #print('hola mundo')
